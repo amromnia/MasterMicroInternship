d9914571d2ba479a12578974be4f1201a8cbd396
Amromnia <amr2000225@miuegypt.edu.eg>
2023-01-09 17:55:47 +0200

    Added most functionality except GUI Added functionality:     -More Validations.     -More Error Handling.     -Migrated to numpy instead of lists.     -Migrated to vectorized function instead of loops.     -Changed to more concise variable names.     -Improved performance.     -Improved readability & changed some code to a more pythonic way.     -Made sure to disallow numpy from using more than 2gb of memory.     -All functionality from point 1 through point 7         in the requirements have been implemented.

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from plotter import *
 import numpy as np
 
 def main():
-    min_value = int(input("Enter the minimum value: "))
-    max_value = int(input("Enter the maximum value: "))
-    function_string = input("Enter the function: ")
+    try:
+        min_value = float(input("Enter the minimum value: "))
+        max_value = float(input("Enter the maximum value: "))
+        function_string = input("Enter the function: ")
+    except ValueError:
+        print("Invalid Input, Input Valid Number or Function")
+        return
+    if min_value>max_value:
+        print("Min Value Must Be Smaller Than Max Value!")
+        return
+    if max_value*np.dtype(np.float64).itemsize>2e9:
+        print("Max Value Too Big, Needs more than 2 gigabytes!")
+        return
     x, y, e= Plotter(min_value, max_value, function_string)
     if e is not None:
         print(e)
--- plotter.py
+++ plotter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import re
+import numpy as np
 
 def Plotter(min_value, max_value, function_string):
     e = None
@@ -7,20 +8,22 @@ def Plotter(min_value, max_value, function_string):
         function_string = sanitize_string(function_string)
     except ValueError as e:
         return [], [], e
-    x_axis = []
-    y_axis = []
-    for i in range(min_value, max_value+1):
-        x_axis.append(i)
-        y_axis.append(eval(function_string, {"x": i}))
+
+    try:
+        x_axis = np.arange(min_value, max_value+1, dtype=np.float64)
+        applied_function = np.vectorize(lambda x: eval(function_string, {"x": x}), otypes=[np.float64])
+        y_axis = applied_function(x_axis)
+    except Exception as e:
+        return [], [], e
     plt.plot(x_axis, y_axis)
     plt.show()
     return x_axis, y_axis, e
 
 def sanitize_string(function_string):
-    for expression in re.findall('[a-zA-Z_]+', function_string):
-        if expression != 'x':
+    for variable in re.findall('[a-zA-Z_]+', function_string):
+        if variable != 'x':
             raise ValueError(
-                f"{expression} is not supported or is not a valid expression."
+                f"{variable} is not supported or is not a valid variable (Only X functions are supported)."
             )
     function_string = function_string.replace('^', '**')
     return function_string
